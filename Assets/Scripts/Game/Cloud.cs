822075da00fe5dc1d5b8fc8fea116a129e1794e5
Osilos <f.marianacci@gmail.com>
2017-05-30 17:17:56 +0200

    create prefab test ld

---
+++ Assets/Scripts/Game/Cloud.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+	private static readonly float zLimit = 300f;
+	private static readonly float zRespawn = -300f;
+
+	[SerializeField]
+	private float speed;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * speed * Time.deltaTime);
+		if (transform.position.z > zLimit)
+			transform.position = new Vector3(transform.position.x, transform.position.y, zRespawn);
+	}
+}
