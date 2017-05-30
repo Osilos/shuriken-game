2b30b4b2e9155822bdd8a9d53114ca3633be572a
Dimitri <d.pomier@student.isartdigital.com>
2017-05-30 12:03:52 +0200

    game manager

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    static GameManager m_instance = null;
+
+    static WaveManager m_waveManager;
+
+    void Awake () {
+        if (m_instance == null) {
+            m_instance = this;
+        } else {
+            Destroy( this );
+            return;
+        }
+
+        m_waveManager = new WaveManager();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_waveManager.Update();
+
+    }
+}
