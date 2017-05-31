293a25f2813c4a2d3e54ca72b5866bb19d545567
Osilos <f.marianacci@gmail.com>
2017-05-31 11:09:05 +0200

    wave syste

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -4,37 +4,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public static Camera mainCamera;
+	public static Camera mainCamera;
 
-    static GameManager m_instance = null;
+	static GameManager m_instance = null;
 
-    static WaveManager m_waveManager;
+	static Wave m_waveManager;
 
-    [Header("Enemies")]
-    public Enemy PrefabEnemy;
-    public GameObject EnemyA;
-    public GameObject EnemyB;
+	[Header("Enemies")]
+	public Enemy PrefabEnemy;
+	public GameObject EnemyA;
+	public GameObject EnemyB;
 
-    void Awake () {
-        if (m_instance == null) {
-            m_instance = this;
-        } else {
-            Destroy( this );
-            return;
-        }
+	void Awake () {
+		if (m_instance == null) {
+			m_instance = this;
+		} else {
+			Destroy( this );
+			return;
+		}
 
-        mainCamera = Camera.main;
-        m_waveManager = new WaveManager( PrefabEnemy, EnemyA, EnemyB );
-    }
+		mainCamera = Camera.main;
+		Wave.Create()
+			.SetOrigin(Vector3.zero)
+			.SetEnemyPrefab(EnemyA)
+			.SetSteps(
+				new StepWave[3] {
+					new StepWave(new Vector3(-5f,0f,5f), 0.5f),
+					new StepWave(new Vector3(0f,0f, 5f), 0.5f),
+					new StepWave(new Vector3(5f,0f,5f), 0.5f)
+				}
+			);
+	}
 
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-        m_waveManager.Update();
-    }
+		
+		//m_waveManager.Update();
+	}
 }
